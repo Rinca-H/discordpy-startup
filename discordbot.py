ecb4a71c4d7a36fc34b0ddae43d87a5ff3698c1c
Rinca <54294393+Rinca-H@users.noreply.github.com>
2020-11-16 08:31:34 +0900

    update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -9,7 +9,18 @@ token = os.environ['DISCORD_BOT_TOKEN']
 @client.event
 async def on_message(message):
     if client.user != message.author:    # 送り主がBotだった場合反応したくないから
-        if message.content.startswith('おはよう'):    # どんな言葉で始まるか調べる
+        if message.content.startswith('/help'):   # 改行コード使用例↓ と、代入するのはmじゃなくてもいい証明
+            desc1 = '\n・おはよう\n・おやすみ\n(・かわいい)\n・生きてる\n・おみくじ\n(・今日の格言)\n・鈴花'
+            embed = discord.Embed(title="反応する言葉",description=desc1, color=0x64ec86)    # embedオブジェクトの定義
+            embed.add_field(name="/say",value="何かを言わせられるよ",inline=False)    # フィールドの追加　横に並べたいときはinlineをtrueに
+            embed.add_field(name="/termux",value="termuxの使い方を教えるよ",inline=False)
+            embed.add_field(name="/howtocheck",value="termuxでのpythonのチェックのしかたを教えるよ",inline=False)
+            embed.add_field(name="/logout",value="ログアウトするよ(使わないで！)",inline=False)
+            embed.add_field(name="/slot",value="スロットをプレイするよ",inline=False)
+            await message.channel.send(embed=embed)
+            
+            
+        elif message.content.startswith('おはよう'):    # どんな言葉で始まるか調べる
             m = 'おはよう！' + message.author.name + 'さん！'    # メッセージを変数に詰め込む
             await message.channel.send(m)    # メッセージが送られてきたチャンネルへメッセージを送る
         
@@ -19,25 +30,34 @@ async def on_message(message):
             await message.channel.send(m)
         
         
+        elif message.content.startswith('生きてる'):
+            m = '生きてるよ～笑'
+            await message.channel.send(m)
+        
+        
+        elif message.content.startswith('おみくじ'):
+            x = random.randint(1, 6)
+            if x == 6:
+                m = 'すごい！あたり！！'
+                await message.channel.send(m)
+            elif x != 6:   # notはこう
+                m = 'ざんねん……はずれ……'
+                await message.channel.send(m)
+        
+        
+        elif '鈴花' in message.content:   # 「鈴花」が含まれてるだけで反応する
+            m = 'なに〜？？よんだ〜？'
+            await message.channel.send(m)
+        
+        
         elif message.content.startswith('/say'):
             m = message.content[5:]    # 5文字目以降を取得
             await message.delete()    #1つ前のメッセージを消す
             await message.channel.send(m)
         
         
-        elif message.content.startswith('/help'):   # 改行コード使用例↓ と、代入するのはmじゃなくてもいい証明
-            desc1 = '\n・おはよう\n・おやすみ\n(・かわいい)\n・生きてる\n・おみくじ\n(・今日の格言)\n・鈴花'
-            embed = discord.Embed(title="反応する言葉",description=desc1, color=0x64ec86)
-            embed.add_field(name="/say",value="何かを言わせられるよ",inline=False)
-            embed.add_field(name="/termux",value="termuxの使い方を教えるよ",inline=False)
-            embed.add_field(name="/howtocheck",value="termuxでのpythonのチェックのしかたを教えるよ",inline=False)
-            embed.add_field(name="/logout",value="ログアウトするよ(使わないで！)",inline=False)
-            embed.add_field(name="/slot",value="スロットをプレイするよ",inline=False)
-            await message.channel.send(embed=embed)
-        
-        
         elif message.content.startswith('/termux'):
-            a = 'termuxで、\n「`$cd /storage/emulated/0/bot`」\n「`$python 〇〇.py`」\nと入力するとそのpythonファイルを起動できるよ'
+            a = 'termuxで、\n「`$cd /storage/emulated/0/bot`」\n「`$python 〇〇.py`」\nのように入力するとそのpythonファイルを起動できるよ'
             await message.channel.send(a)
         
         
@@ -52,27 +72,6 @@ async def on_message(message):
             await message.channel.send(m)
             await client.logout()    # ログアウト
             await sys.exit()    # シャットダウン
-        
-        
-        elif message.content.startswith('生きてる'):
-            m = '生きてるよ～笑'
-            await message.channel.send(m)
-        
-        
-        elif '鈴花' in message.content:   # 「鈴花」が含まれてるだけで反応する
-            m = 'なに〜？？よんだ〜？'
-            await message.channel.send(m)
-        
-        
-        elif message.content.startswith('おみくじ'):
-            x = random.randint(1, 6)
-            if x == 6:
-                m = 'すごい！あたり！！'
-                await message.channel.send(m)
-            elif x != 6:   # notはこう
-                m = 'ざんねん……はずれ……'
-                await message.channel.send(m)
-        
         
         
         elif message.content.startswith('/slot'):
