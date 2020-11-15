8ee73ee625b702d2c0c5e1476954878d3ac59c1f
Rinca <54294393+Rinca-H@users.noreply.github.com>
2020-11-15 21:30:48 +0900

    update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,21 +1,20 @@
-from discord.ext import commands
+import discord
+import asyncio
+import random
+import sys
 import os
-import traceback
 
-bot = commands.Bot(command_prefix='/')
+client = discord.Client()
 token = os.environ['DISCORD_BOT_TOKEN']
+channel_id = 659621491775635507
+channel = client.get_channel(channel_id)
 
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
+@client.event
+async def on_message(message):
+    if client.user != message.author:   # 送り主がBotだった場合反応したくないから
+        if message.content.startswith('おはよう'):   # どんな言葉で始まるか調べる
+            m = 'おはよう！' + message.author.name + 'さん！'   # メッセージを変数に詰め込む
+            await message.channel.send(m)   # メッセージが送られてきたチャンネルへメッセージを送る
 
 bot.run(token)
