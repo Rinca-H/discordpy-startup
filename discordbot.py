1010cfad09c0ad28d318c16d51b4d3e18eec8687
Rinca <54294393+Rinca-H@users.noreply.github.com>
2020-11-15 21:48:54 +0900

    update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,28 +1,48 @@
-from discord.ext import commands
+import discord
+import asyncio
+import random
+import sys
 import os
-import traceback
+import botFunction.functions as f
 
-bot = commands.Bot(command_prefix='/')
-token = os.environ['DISCORD_BOT_TOKEN']
+client = discord.Client()
+
+func_list = {
+    '名言': f.random_meigen,
+    '迷言': f.random_meigen,
+    '武器': f.random_splat_buki,
+    'ブキ': f.random_splat_buki,
+    'help': f.help,
+    'ヘルプ': f.help
+}
 
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+@client.event
+async def on_ready():
+    '''
+    起動時に呼ばれるメソッド
+    '''
+    print('-----Logged in info-----')
+    print(client.user.name)
+    print(client.user.id)
+    print('------------------------')
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
 
-@bot.command()
-async def hello(ctx):
-    await ctx.send('hello!')
-
-@bot.event
+@client.event
 async def on_message(message):
-    if message.content.startswith('test'):
-        await ctx.send('test')
+    '''
+    特定のメッセージを受け取って処理する\n
+    今はメンションを送るとランダムに名言を返す
+    '''
+    try:
+        if client.user.id in message.content:
+            for k in func_list:
+                if k in str(message.content):
+                    await func_list[k](client, message)
+                    break
+            else:
+                await f.random_reply(client, message)
+    except:
+        print(sys.exc_info())
 
-bot.run(token)
+client.run(os.environ.get('DISCORD_BOT_TOKEN'))
