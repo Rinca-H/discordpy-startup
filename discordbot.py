88dd4be242b309ad8367fbad3620f02b0553076c
Rinca <54294393+Rinca-H@users.noreply.github.com>
2020-11-15 21:13:20 +0900

    update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -17,5 +17,11 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pong')
 
+@bot.command()
+async def on_message(message):
+    if bot.user != message.author:   # 送り主がBotだった場合反応したくないから
+        if message.content.startswith('おはよう'):   # どんな言葉で始まるか調べる
+            m = 'おはよう！' + message.author.name + 'さん！'   # メッセージを変数に詰め込む
+            await message.channel.send(m)   # メッセージが送られてきたチャンネルへメッセージを送る
 
 bot.run(token)
