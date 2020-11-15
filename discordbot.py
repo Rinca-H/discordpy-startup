e7c5ce8509f9a77516477e4055d063e255b97bcc
Rinca <54294393+Rinca-H@users.noreply.github.com>
2020-11-16 07:42:41 +0900

    update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,24 +1,16 @@
-from discord.ext import commands
+import discord
+import random
+import json
 import os
-import traceback
 
-bot = commands.Bot(command_prefix='/')
+client = discord.Client()
 token = os.environ['DISCORD_BOT_TOKEN']
 
+@client.event
+async def on_message(message):
+    if client.user != message.author:
+        if message.content.startswith('おはよう'):
+            m = 'おはよう！' + message.author.name + 'さん！'
+            await message.channel.send(m)
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
-@bot.command()
-async def hello(ctx):
-    await ctx.send('hello!')
-
-
-bot.run(token)
+client.run(token)
